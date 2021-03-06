2ef8c47b51cf6a4163329fa01ac24b696b084d47
cy6x <73636513+cy6x@users.noreply.github.com>
2021-03-06 22:49:50 +0300

    Update install.cmd

--- Windows/install.cmd
+++ Windows/install.cmd
@@ -1,4 +1,4 @@
 robocopy qBittorrent %appdata%\qBittorrent
 
-xcopy font\Terminu.ttf %windir%\Fonts
+xcopy Terminal\font\Terminu.ttf %windir%\Fonts
 robocopy Terminal %localappdata%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
