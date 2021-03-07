650e04d036f973544713c156f6747cdde00470e2
cy6x <73636513+cy6x@users.noreply.github.com>
2021-03-07 21:51:33 +0300

    Update install.ps1

--- Windows/install.ps1
+++ Windows/install.ps1
@@ -1,4 +1,7 @@
 $ProgressPreference = 'SilentlyContinue'
 
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cy6x/dotfiles/main/Windows/qBittorrent/qBittorrent.ini" -OutFile (New-Item -Path "$env:APPDATA\qBittorrent\qBittorrent.ini" -Force)
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cy6x/dotfiles/main/Windows/Terminal/font/Terminus.ttf" -OutFile "$env:windir\Fonts\Terminus.ttf"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cy6x/dotfiles/main/Windows/Terminal/settings.json" -OutFile (New-Item -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Force)
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cy6x/dotfiles/main/Windows/Terminal/font/Terminus.ttf" -OutFile (New-Item -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Force)
