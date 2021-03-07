f52c97a45078b9c8a47acdf636392f5db9ddc43a
cy6x <73636513+cy6x@users.noreply.github.com>
2021-03-07 21:41:32 +0300

    Update install.ps1

--- Windows/install.ps1
+++ Windows/install.ps1
@@ -1,1 +1,3 @@
+$ProgressPreference = 'SilentlyContinue'
+
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cy6x/dotfiles/main/Windows/qBittorrent/qBittorrent.ini" -OutFile (New-Item -Path "$env:APPDATA\qBittorrent\qBittorrent.ini" -Force)
