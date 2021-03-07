cdcf46eed002d6de0e6e9a855477aa6d53b60b64
cy6x <73636513+cy6x@users.noreply.github.com>
2021-03-07 21:39:22 +0300

    Update install.ps1

--- Windows/install.ps1
+++ Windows/install.ps1
@@ -1,1 +1,1 @@
-Write-Host "Hello, World!"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cy6x/dotfiles/main/Windows/qBittorrent/qBittorrent.ini" -OutFile "$env:APPDATA\qBittorrent\qBittorrent.ini"
