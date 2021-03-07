144bc413af03b0c5c0b0f8debf8de42987ee4779
cy6x <73636513+cy6x@users.noreply.github.com>
2021-03-07 22:01:18 +0300

    Update install.ps1

--- Windows/install.ps1
+++ Windows/install.ps1
@@ -5,7 +5,6 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cy6x/dotfiles/main/Win
 
 # Windows Terminal
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cy6x/dotfiles/main/Windows/Terminal/settings.json" -OutFile (New-Item -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Force)
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cy6x/dotfiles/main/Windows/Terminal/font/Terminus.ttf" -OutFile (New-Item -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Force)
 
 # Terminus font
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cy6x/dotfiles/main/Windows/Terminal/font/Terminus.ttf" -OutFile "$env:windir\Fonts\Terminus.ttf"
