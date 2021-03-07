16024b08811a7687c51ff9d2230055943c622292
cy6x <73636513+cy6x@users.noreply.github.com>
2021-03-07 21:41:52 +0300

    Delete install.cmd

--- Windows/install.cmd
+++
@@ -1,5 +0,0 @@
-robocopy qBittorrent %appdata%\qBittorrent
-
-xcopy Terminal\font\Terminus.ttf %windir%\Fonts
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "FontName (TrueType)" /t REG_SZ /d Terminus.ttf /f
-robocopy Terminal %localappdata%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
