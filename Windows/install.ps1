b58a2b460adfb1aaeeb726cbfe653632eb6cda1f
cy6x <73636513+cy6x@users.noreply.github.com>
2021-03-08 00:18:09 +0300

    Update install.ps1

--- Windows/install.ps1
+++ Windows/install.ps1
@@ -1,11 +1,11 @@
 $ProgressPreference = 'SilentlyContinue'
 
-# qBittorrent
+# Configure qBittorrent
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cy6x/dotfiles/main/Windows/qBittorrent/qBittorrent.ini" -OutFile (New-Item -Path "$env:APPDATA\qBittorrent\qBittorrent.ini" -Force)
 
-# Windows Terminal
+# Configure Windows Terminal
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cy6x/dotfiles/main/Windows/Terminal/settings.json" -OutFile (New-Item -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Force)
 
-# Terminus font
+## Install Terminus font
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cy6x/dotfiles/main/Windows/Terminal/font/Terminus.ttf" -OutFile "$env:windir\Fonts\Terminus.ttf"
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name "Terminus (TrueType)" -Type String -Value "Terminus.ttf" -Force | Out-Null
